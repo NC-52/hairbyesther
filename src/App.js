742218dd4f2dc4b2c51326fc68262185d4370725
leonel47 <waboleonel@gmail.com>
2021-07-25 13:14:35 -0400

    Adding presentation slider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css'
 import Header from './components/header/Header'
+import React from "react";
+import PresentationSlider from "./components/slide/PresentationSlider";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <PresentationSlider />
     </div>
   )
 }
