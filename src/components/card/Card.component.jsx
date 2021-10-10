c4df5e6f197620b2e4e3332b36f30a6985dba148
leonel47 <waboleonel@gmail.com>
2021-10-10 08:31:21 -0400

    Styling product item card

--- src/components/card/Card.component.jsx
+++ src/components/card/Card.component.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
-import './card.style.scss'
+import './card.style.css'
+import img from './product_05.jpg'
 
 const CardComponent = () => {
     return (
-        <div className="col-4">
-            <div className="view overlay">
-                <img src="https://mdbootstrap.com/img/new/standard/city/059.jpg"
-                     className="card-img-top"
-                     alt="Card city"/>
+        <div className="col-lg-4 col-md-4 all gra product-card-container">
+            <div className='product-card-content'>
+                <img src={img} className="product-card-image" alt=""/>
+                <div className="name-n-price">
+                    <h4>Product name</h4>
+                    <span>$12.5</span>
+                </div>
             </div>
         </div>
     )
--- src/components/pages/landing/LandingPage.jsx
+++ src/components/pages/landing/LandingPage.jsx
@@ -43,7 +43,7 @@ const LandingPage = () => {
         <>
             <ImageSlider sliderObject={slideObject}/>
             {/*<PresentationSlider/>*/}
-            {/*<ProductList/>*/}
+            <ProductList/>
             {/*<AuthorPresentation/>*/}
         </>
     )
