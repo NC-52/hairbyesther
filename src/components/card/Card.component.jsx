d0cd99b7aea61b477007b2801dd183f987dfee4b
leonel47 <waboleonel@gmail.com>
2021-10-21 08:38:51 -0400

    Code refactoring, removing comments

--- src/components/card/Card.component.jsx
+++ src/components/card/Card.component.jsx
@@ -4,7 +4,7 @@ import img from './product_05.jpg'
 
 const CardComponent = () => {
     return (
-        <div className="col-lg-4 col-md-4 all gra product-card-container">
+        <div className="col-lg-4 col-md-4 col-xl-3 all gra product-card-container">
             <div className='product-card-content'>
                 <img src={img} className="product-card-image" alt=""/>
                 <div className="name-n-price">
